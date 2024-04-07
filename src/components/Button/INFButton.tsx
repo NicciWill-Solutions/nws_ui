543e5ee5dbc8dceb451ff6802a7a3d9777b48724
Nichole Williams <nicci@nicciwill.com>
2024-04-07 18:46:11 -0400

    add flowbite. refactor button to use it

--- src/components/Button/INFButton.tsx
+++ src/components/Button/INFButton.tsx
@@ -1,43 +1,21 @@
-import React from 'react';
+import {
+  ButtonProps as FlowButtonProps,
+  Button as FlowButton,
+} from "flowbite-react";
 
 // Define an interface for the button's props
-interface ButtonProps {
-  children: React.ReactNode; // Specifies that children can be any valid React node
-  variant?: 'primary' | 'secondary'; // Optional variant prop, with specific allowed values
-  size?: 'sm' | 'md' | 'lg'; // Optional size prop, with specific allowed values
-  onClick?: () => void; // Optional onClick event handler
+interface ButtonProps extends FlowButtonProps {
+  children?: React.ReactNode; // Specifies that children can be any valid React node
+  color?: 'primary' | 'secondary' | 'error' | 'outline' | 'warning' | undefined; // Optional variant prop, with specific allowed values
+  size?: 'sm' | 'md' | 'lg' | undefined; // Optional size prop, with specific allowed values
 }
 
 // Button component with TypeScript
-const INFButton: React.FC<ButtonProps> = ({
-  children='Hello, World! I\'m a button!',
-  variant = 'primary',
-  size = 'md',
-  ...props
-}) => {
-  // Define base classes for the button
-  const baseClasses = 'focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center';
-
-  // Variant classes
-  const variantClasses = {
-    primary: 'bg-blue-600 hover:bg-blue-700 text-white',
-    secondary: 'bg-gray-300 hover:bg-gray-400 text-black',
-  };
-
-  // Size classes
-  const sizeClasses = {
-    sm: 'text-xs px-3 py-1.5',
-    md: 'text-sm px-5 py-2.5',
-    lg: 'text-lg px-7 py-3',
-  };
-
-  // Combine classes based on props
-  const classes = `${baseClasses} ${variantClasses[variant]} ${sizeClasses[size]}`;
-
+const INFButton = ({children, color, size = 'md', ...otherProps}: ButtonProps) => {
   return (
-    <button {...props} className={classes}>
+    <FlowButton color={color} {...otherProps} >
       {children}
-    </button>
+    </FlowButton>
   );
 };
 
